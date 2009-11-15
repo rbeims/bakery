52b0cc60caa1323601f1e4a4511e62fcba96ae52
Esben Haabendal <eha@doredevelopment.dk>
2009-11-15 23:01:10 +0100

    fix hack to be able to run from source directory

--- oebakery/oe.py
+++ oebakery/oe.py
@@ -30,8 +30,8 @@ specific command."""
             return
 
     # hack to be able to run from source directory
-    sys.path.insert(0,os.path.join(
-            os.path.dirname(os.path.realpath(sys.argv[0])), '..'))
+    sys.path.insert(
+        0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
     import oebakery
     from oebakery.cmd_init import InitCommand
