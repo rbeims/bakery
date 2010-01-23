a4105aac84ee00bbe0ac6c8d703389309a4ea000
Esben Haabendal <eha@doredevelopment.dk>
2010-01-23 12:17:10 +0100

    Added more options to pull command
    * --main
    * --update-submodules
    * --remotes
    * --all-submodules
    * [submodule]*

--- src/oebakery/cmd_pull.py
+++ src/oebakery/cmd_pull.py
@@ -7,19 +7,46 @@ class PullCommand:
 
     def __init__(self, config, argv=[]):
 
-        parser = optparse.OptionParser("""Usage: oe pull [options]
+        parser = optparse.OptionParser("""Usage: oe pull [options] [submodules]*
 
-  Fetch from and merge with remote repositories.""")
+  Fetch from remote repositories.
 
-        parser.add_option("-r", "--update-remotes",
+  When called without submodules, fetch and merge head of main repository,
+  and checkout the submodule versions stored in main repository.
+
+  When called with a list of submodules, fetch and merge branch heads of
+  specified submodules, overriding the versions stored in main repository.
+  Main repository is not pulled.""")
+
+        parser.add_option("-m", "--main",
+                          action="store_true", dest="main", default=False,
+                          help="pull branch head of main repository")
+
+        parser.add_option("-u", "--update-submodules",
+                          action="store_true", dest="update_sub", default=False,
+                          help="checkout submodule versions specified in main repository")
+
+        parser.add_option("-r", "--remotes",
                           action="store_true", dest="remotes", default=False,
-                          help="update remotes (tracked repositories)")
+                          help="fetch updates for remote tracking branches")
 
-        parser.add_option("-s", "--pull-submodules",
-                          action="store_true", dest="submodules", default=False,
-                          help="pull submodule branch head")
+        parser.add_option("-s", "--all-submodules",
+                          action="store_true", dest="all_sub", default=False,
+                          help="pull all submodule branch heads (and main repository)")
 
         (self.options, self.args) = parser.parse_args(argv)
+
+        if len(argv) == 0:
+            self.options.main = True
+            self.options.update_sub = True
+
+        if self.options.update_sub and self.options.all_sub:
+            print 'Invalid arguments: --update-submodules and --all-submodules'
+            return
+
+        if self.options.all_sub and len(self.args) > 0:
+            print 'Invalid arguments: --all-submodules and submodule(s)'
+            return
 
         self.config = config
 
@@ -32,45 +59,71 @@ class PullCommand:
             print 'Aiee!  This is not a git repository!!'
             return
 
-        if not oebakery.call('git pull'):
-            print 'Failed to pull updates to main repository'
-
         if self.options.remotes:
             self.git_remote_update()
 
-        if not self.options.submodules and os.path.exists('.gitmodules'):
-            if not oebakery.call('git submodule update --init'):
+        if self.options.main:
+            if not oebakery.call('git pull'):
+                print 'Failed to pull updates to main repository'
+
+        if self.options.update_sub and os.path.exists('.gitmodules'):
+            if not oebakery.call('git submodule update --init --recursive'):
                 print 'Failed to update git submodules'
                 return
 
         # get list of submodules
-        submodules = {}
-        lines = oebakery.call('git submodule status', quiet=True).split('\n')
-        for line in lines:
+        self.submodules = {}
+        for line in oebakery.call('git submodule status', quiet=True)\
+                .split('\n'):
             if len(line) == 0:
                 continue
+    
             path = line[1:].split()[1]
             prefix = line[0]
             commitid = line[1:].split()[0]
-
+    
+            fetch_url = None
+            push_url = None
+            branch = None
+            remotes = None
+    
             if self.config.has_option('submodules', path):
                 url = self.config.get('submodules', path)
-
+    
                 url_split = url.split()
                 if len(url_split) < 1 or len(url_split) > 3:
                     print 'Invalid submodule url (%s): %s'%(path, url)
                     return
-
+    
+                fetch_url = url_split[0]
+    
                 if len(url_split) > 1:
                     branch = url_split[1]
-                else:
-                    branch = None
+    
+                if len(url_split) > 2:
+                    push_url = url_split[2]
+    
+                section_name = 'remotes "%s"'%path
+                if self.config.has_section(section_name):
+                    remotes = self.config.items(section_name)
+    
+            self.submodules[path] = {
+                'prefix'	: prefix,
+                'commitid'	: commitid,
+                'fetch_url'	: fetch_url,
+                'push_url'	: push_url,
+                'branch'	: branch,
+                'remotes'	: remotes,
+                }
 
-                if self.options.submodules:
-                    self.git_pull_submodule(path, branch)
+        for path in self.submodules.keys():
+            if self.options.remotes:
+                self.git_remote_update(path)
 
-                if self.options.remotes:
-                    self.git_remote_update(path)
+        for path in self.submodules.keys():
+            if self.options.all_sub or path in self.args:
+                self.git_pull_submodule(path, self.submodules[path]['branch'])
+
         return
 
 
