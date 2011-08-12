65fea676fe93e3832d5a4b2e70ee660a4b14a953
Esben Haabendal <eha@doredevelopment.dk>
2011-08-12 12:40:47 +0200

    Fix variable expansion support.

--- src/oebakery/data.py
+++ src/oebakery/data.py
@@ -1,3 +1,5 @@
+import re
+import types
 
 
 NO_EXPANSION      = 0
@@ -21,25 +23,60 @@ class ExpansionStack:
         del self.stack[-1:]
         return
 
+    def __len__(self):
+        return len(self.stack)
+
+    def __str__(self, prefix="  "):
+        return prefix + ("\n%s"%(prefix)).join(self.stack)
+
 
 class BakeryData:
 
     def __init__(self):
         self._dict = {}
         self.mtime = {}
+        self.expand_stack = ExpansionStack()
         return
 
     def dict(self):
         return self._dict
 
-    def set(self, var, value, expand=0):
+    def set(self, var, value):
         return self.set_flag(var, "", value)
 
-    def get(self, var, expand=2):
-        val = self.get_flag(var, "")
-        if expand and val:
-            val = self.expand(val, expand in (1, True))
-        return val
+    def get(self, var, expand=FULL_EXPANSION):
+        return self._get(var, expand)[0]
+
+    def _get(self, var, expand=FULL_EXPANSION):
+        #print "_get var=%s expand=%s"%(var, expand)
+        assert isinstance(expand, int)
+        try:
+            val = self._dict[var][""]
+        except KeyError:
+            try:
+                val = self._dict[var]["defaultval"]
+            except KeyError:
+                val = None
+        if not expand:
+            return (val, None)
+        if not var in self._dict:
+            return (None, None)
+        if not isinstance(val, (basestring, types.NoneType)):
+            return (val, None)
+
+        deps = set()
+        #print "get expanding %s=%s"%(var, repr(val))
+        expand_method = self.get_flag(var, "expand")
+        if expand_method:
+            expand_method = int(expand_method)
+        else:
+            expand_method = expand
+        if expand_method != NO_EXPANSION and val:
+            self.expand_stack.push("${%s}"%var)
+            (val, deps) = self._expand(val, expand_method)
+            self.expand_stack.pop()
+
+        return (val, deps)
 
     def set_flag(self, var, flag, value):
         if not var in self._dict:
@@ -67,12 +104,13 @@ class BakeryData:
             pass
         return
 
-    def append(self, var, value, separator=" "):
-        current = self.get(var)
-        if current == None:
-            self.set(var, value)
-        else:
-            self.set(var, current + separator + value)
+    #def append(self, var, value, separator=" "):
+    #    print "append var=%s value=%s seperator=%s"%(var, value, separator)
+    #    current = self.get(var)
+    #    if current == None:
+    #        self.set(var, value)
+    #    else:
+    #        self.set(var, current + separator + value)
 
     def append_flag(self, var, flag, value, separator=" "):
         current = self.get_flag(var, flag)
@@ -81,12 +119,13 @@ class BakeryData:
         else:
             self.set_flag(var, flag, current + separator + value)
 
-    def prepend(self, var, value, separator=" "):
-        current = self.get(var)
-        if current == None:
-            self.set(var, value)
-        else:
-            self.set(var, value + separator + current)
+
+    #def prepend(self, var, value, separator=" "):
+    #    current = self.get(var)
+    #    if current == None:
+    #        self.set(var, value)
+    #    else:
+    #        self.set(var, value + separator + current)
 
     def prepend_flag(self, var, flag, value, separator=" "):
         current = self.get_flag(var, flag)
@@ -96,7 +135,7 @@ class BakeryData:
             self.set_flag(var, flag, value + separator + current)
 
 
-    #def set_file_mtime(self, filename, bbpath, mtime):
+    #def set_file_mtime(self, filename, path, mtime):
     def set_file_mtime(self, filename, mtime):
         self.mtime[filename] = mtime
 
@@ -154,16 +193,15 @@ class BakeryData:
         method -- Expansion method (default: FULL_EXPANSION)
     
         Expansion methods:
-        EXPAND_NONE -- no recursive expansion
-        EXPAND_FULL -- full expansion, all variables must be expanded
+        NO_EXPANSION -- no recursive expansion
+        FULL_EXPANSION -- full expansion, all variables must be expanded
         """
-        print "expand method=%s string=%s"%(method, repr(string))
+        #print "expand method=%s string=%s"%(method, repr(string))
         (new_string, deps) = self._expand(string, method)
         return new_string
 
-
     def _expand(self, string, method):
-        print "_expand method=%s string=%s"%(method, repr(string))
+        #print "_expand method=%s string=%s"%(method, repr(string))
         orig_string = string
         var_re    = re.compile(r"\${[^@{}]+}")
         python_re = re.compile(r"\${@.+?}")
@@ -173,6 +211,10 @@ class BakeryData:
         for var_match in var_re.finditer(string):
             var = var_match.group(0)[2:-1]
             (val, recdeps) = self._get(var)
+            if val is None:
+                if method == FULL_EXPANSION:
+                    raise Exception("Cannot expand variable ${%s}"%(var),
+                                    self.expand_stack)
             expanded_string += (string[string_ptr:var_match.start(0)] +
                                 "%s"%(val,))
             string_ptr = var_match.end(0)
@@ -180,5 +222,5 @@ class BakeryData:
             if recdeps:
                 deps.union(recdeps)
         expanded_string += string[string_ptr:]
-        print "returning expanded string %s"%(repr(expanded_string))
+        #print "returning expanded string %s"%(repr(expanded_string))
         return (expanded_string, deps)
