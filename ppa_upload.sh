0688327b6db36c06e4227888eed6d83fdc15a918
Esben Haabendal <esben.haabendal@prevas.dk>
2012-10-24 11:37:28 +0200

    Add experimental quantal (12.10) support
    Signed-off-by: Esben Haabendal <esben.haabendal@prevas.dk>

--- ppa_upload.sh
+++ ppa_upload.sh
@@ -5,11 +5,12 @@ version=`grep -e '^version = ' setup.py|sed -e "s%.*'\(.*\)'.*%\1%;"`
 eval `gpg-agent --daemon`
 
 LEAD_DISTRO="precise"
-OLD_DISTROS="lucid maverick natty oneiric"
+OLD_DISTROS="lucid maverick natty oneiric quantal"
 
 export DH_ALWAYS_EXCLUDE=.git
 
 for distro in $LEAD_DISTRO $OLD_DISTROS ; do
+  echo "Building for $distro"
   if [ -e debian/control.$distro ] ; then
     ln -sf control.$distro debian/control
   else
@@ -17,9 +18,11 @@ for distro in $LEAD_DISTRO $OLD_DISTROS ; do
   fi
   debchange --distribution ${distro} --newversion ${version}~${distro} -b ${distro} build
   debuild -S -k5B997C4F -sa
+  echo
 done
 
 cd ..
 for distro in $LEAD_DISTRO $OLD_DISTROS ; do
+  print "Uploading $distro"
   dput ppa:esben-haabendal/oe-lite oe-lite_${version}~${distro}_source.changes
 done
